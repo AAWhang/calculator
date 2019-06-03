163dd4b9708572fdb4b5e70ba74aa7735c228ad4
Aaron Whang and Joseph Tubridy <joseph@>
2019-06-03 12:03:31 -0700

    finished javascript to delete first and last character off string

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,98 @@
-//addition
-var add = function(number1 , number2){
-	return number1 + number2;
-};
 
-var number1 = parseInt(prompt("enter a number."));
-var number2 = parseInt(prompt("enter another number."));
+$(document).ready(function()
+  {
+/*
+    //addition
+    var add = function(number1 , number2){
+    	return number1 + number2;
+    };
 
-
-alert(add(number1, number2));
-
-
-//bmi calculator
-var add = function(weight , height){
-	return weight / (height * height) * 703;
-};
-
-var weight = parseInt(prompt("What is your weight in pounds?"));
-var height = parseInt(prompt("What is your height in inches?"));
+    var number1 = parseInt(prompt("enter a number."));
+    var number2 = parseInt(prompt("enter another number."));
 
 
+    alert(add(number1, number2));
 
 
-//celsius to fahrenheit conversion
-var convert = function (tempC){
-  return (tempC * (9/5)) + 32;
-}
+    //bmi calculator
+    var add = function(weight , height){
+    	return weight / (height * height) * 703;
+    };
 
-var tempC= parseInt(prompt("what temperature is it in celcius?"));
- alert("It is " + convert(tempC) + "F.");
+    var weight = parseInt(prompt("What is your weight in pounds?"));
+    var height = parseInt(prompt("What is your height in inches?"));
 
 
 
-//gallon to liters conversion
- var gallon2liters = function (gallon){
-   return gallon * 3.785;
- }
 
- var gallon= parseInt(prompt("How many gallons?"));
-  alert("It is " + gallon2liters(gallon) + "L.");
+    //celsius to fahrenheit conversion
+    var convert = function (tempC){
+      return (tempC * (9/5)) + 32;
+    }
 
-
-
-//ounce to grams conversion
-var ounce2Grams = function(ounce){
-  return ounce * 28.3495;
-}
-
-var ounce=parseInt(prompt("How many ounces?"));
-alert("there are " + ounce2Grams(ounce) + " grams.");
+    var tempC= parseInt(prompt("what temperature is it in celcius?"));
+     alert("It is " + convert(tempC) + "F.");
 
 
 
-//asks name age and food and returns all info in a string
-var person = function(name , age , food){
-	return sayinfo;
-};
+    //gallon to liters conversion
+     var gallon2liters = function (gallon){
+       return gallon * 3.785;
+     }
 
-var name = prompt("What is your name?");
-var age = prompt("What is your age?");
-var food = prompt("what is your favorite food?");
+     var gallon= parseInt(prompt("How many gallons?"));
+      alert("It is " + gallon2liters(gallon) + "L.");
 
-var sayinfo = alert("Your name is " + name + " and you are " + age + " years old and your favorite food is " + food + ".");
+
+
+    //ounce to grams conversion
+    var ounce2Grams = function(ounce){
+      return ounce * 28.3495;
+    }
+
+    var ounce=parseInt(prompt("How many ounces?"));
+    alert("there are " + ounce2Grams(ounce) + " grams.");
+
+
+
+    //asks name age and food and returns all info in a string
+    var person = function(name , age , food){
+    	return sayinfo;
+    };
+
+    var name = prompt("What is your name?");
+    var age = prompt("What is your age?");
+    var food = prompt("what is your favorite food?");
+
+    var sayinfo = alert("Your name is " + name + " and you are " + age + " years old and your favorite food is " + food + ".");
+
+*/
+
+    //CLICK HEADER PRACTICE
+    $("h1").click(function(){
+    alert("AAAAAAAAAA");
+  });
+
+  var person = function(sentence1){
+    return sayinfo;
+  };
+
+  var sentence1 = prompt("Enter a string");
+  var ommitstr = sentence1.length - 2;
+  alert(ommitstr); //debug test
+
+  var sayinfo = alert(sentence1.substr(1 , ommitstr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
